f3a392ab953aa97012ac5e4066e4bc0f30e9be86
Giwayume <Giwayume@users.noreply.github.com>
2020-04-27 10:40:18 -0400

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,61 @@
+// ==UserScript==
+// @name         Unfuck the Internet
+// @namespace    Unfuck the Internet
+// @version      1
+// @description  Fixes annoying things about various websites on the internet
+// @author       Giwayume
+// @match        *://*/*
+// @grant        none
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+    
+    const domain = window.location.hostname.split('.').slice(-2).join('.');
+    
+    /*-----------------*\
+    | | instagram.com | |
+    \*-----------------*/
+    
+    else if (domain === 'instagram.com') {
+        document.addEventListener('DOMContentLoaded', () => {
+            document.getElementById('react-root').addEventListener('mousedown', (e) => {
+                if (e.target && e.target.hasAttribute('href') && e.target.getAttribute('href').startsWith('/p/')) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    window.location.href = e.target.getAttribute('href');
+                }
+            });
+        });
+    }
+    
+    /*---------------*\
+    | | youtube.com | |
+    \*---------------*/
+    
+    else if (domain === 'youtube.com') {
+        // Auto-accept "Are you still watching?" toasts.
+        setInterval(function() {
+            'use strict';
+            if (document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer').length >= 1) {
+                for (let i = 0; i < document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer').length; i++) {
+                    if (document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer')[i].innerText == "Video paused. Continue watching?") {
+                        document.getElementsByClassName('line-text style-scope yt-confirm-dialog-renderer')[i].parentNode.parentNode.parentNode.querySelector('#confirm-button').click()
+                    }
+                }
+            }
+            (() => {
+                const toasts = document.querySelectorAll('.toast-button');
+                toasts.forEach((toast) => {
+                    if (toast.textContent.includes('Still watching?')) {
+                        toast.querySelector('paper-button').click();
+                        setTimeout(() => {
+                            document.querySelector('paper-toast').remove();
+                        }, 10);
+                    }
+                });
+            })();
+        }, 10)();
+    }
+    
+})();
